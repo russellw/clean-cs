b404a8066b8abfb73f0a0c48bdef638382169fc1
Russell Wallace <russell.wallace@gmail.com>
2023-12-14 17:10:11 +0000

    SortCaseSections

--- SortCaseSections.cs
+++ SortCaseSections.cs
@@ -4,6 +4,11 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 sealed class SortCaseSections: CSharpSyntaxRewriter {
 	public override SyntaxNode? VisitSwitchStatement(SwitchStatementSyntax node) {
-		return base.VisitSwitchStatement(node);
+		var v = new List<SwitchSectionSyntax>(node.Sections);
+		var old = new List<SwitchSectionSyntax>(v);
+		v.Sort((a, b) => string.CompareOrdinal(a.ToString(), b.ToString()));
+		if (v.SequenceEqual(old))
+			return base.VisitSwitchStatement(node);
+		return node.WithSections(new SyntaxList<SwitchSectionSyntax>(v));
 	}
 }
